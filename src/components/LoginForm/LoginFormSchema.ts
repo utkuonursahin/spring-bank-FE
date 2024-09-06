bc856d17784545bb0307c4f28ccb1750cd00d676
utkuonursahin <sahinutkuonur@gmail.com>
2024-09-06 17:15:52 +0300

    feat: implement login page

---
+++ src/components/LoginForm/LoginFormSchema.ts
@@ -0,0 +1,10 @@
+"use client"
+
+import {z} from "zod"
+
+const loginFormSchema = z.object({
+    ssn: z.string().min(12).max(12),
+    password: z.string().min(12)
+})
+
+export default loginFormSchema
